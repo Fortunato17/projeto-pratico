edd5d566096144a59d62b52c94a81d6a11eb6b71
Fortunato17 <148354397+Fortunato17@users.noreply.github.com>
2025-07-08 17:07:29 +0100

    Instalação de biblioteca para notificações(toast)

--- app-filas/src/App.jsx
+++ app-filas/src/App.jsx
@@ -1,15 +1,43 @@
 import { useState } from "react";
+import Teste from "./components/teste";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
-  const [msg, setMsg]=useState(`Olá mundo!`);
-  function mudarMsg(){
-    setMsg(`Mudou a mensagem!`);
+  const [msg, setMsg] = useState(`Olá mundo!`);
+  const [n, setN] = useState(true);
+  function mudarMsg() {
+    //setMsg(msg === "Olá mundo!" ? "Mudou a mensagem!" : "Olá mundo!");
+    if (n) {
+      setMsg("Mudou a mensagem!");
+    } else {
+      setMsg("Olá mundo!");
+    }
+    setN(!n);
   }
-  return(
-    <div className="flex flex-col items-center justify-center h-screen gap-20
-    ">
-      <h1 className="text-5xl font-bold text-blue-500 text-center">{msg} </h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={mudarMsg}>Mudar</button>
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-screen gap-20
+    "
+    >
+      <Toaster position="top-bottom" />;
+      <h1 className="text-5xl font-bold text-[--cor-primaria] text-center">
+        {msg}
+      </h1>
+      <button
+        className="bg-[--cor-primaria] hover:bg-[--cor-verdeClaro] text-white font-bold py-2 px-4 rounded"
+        onClick={()=>{
+          mudarMsg();
+          toast.success(<div className="">
+            <h1 className="text-2xl font-bold mb-2 text-[--cor-primaria]">Fila criada com sucesso!</h1>
+            <p className="mb-2"><span className="font-bold text-[--cor-primaria]">Importante:</span> Copie e guarde o código do seu ticket abaixo. Ele é a única forma de acessar sua fila e não poderá ser recuperado caso seja perdido.</p>
+            <p>Seu código: <span className="font-bold text-[--cor-primaria]">FSAN3</span></p>
+          </div>,{duration: 5000},
+          {icon:""});
+        }} 
+      >
+        Mudar
+      </button>
+      <Teste />
     </div>
   );
 }
